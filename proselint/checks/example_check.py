dd4434ccaf21fc8311c54df8798b6018253f58bb
Lara A. Ross <lara@amperser.com>
2015-01-13 21:53:25 -0800

    Clean up command line utility

---
+++ proselint/checks/example_check.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""ERR100: First line is always wrong.
+
+---
+layout:     post
+error_code: ERR100
+source:     Nobody
+source_url: ???
+title:      ERR100&#58; Firse line is always wrong.
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+The first line always is always wrong.
+
+"""
+
+
+def check(text):
+
+    error_code = "PL000"
+    msg = "First line always has an error."
+
+    return [(1, 1, error_code, msg)]
+
+
+def test_pass():
+    return True
+
+
+def test_fail():
+    return False
