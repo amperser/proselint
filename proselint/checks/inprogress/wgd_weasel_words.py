c4098b49ca36f892311afb5701ecc6e1325697c8
Jordan Suchow <suchow@berkeley.edu>
2015-01-22 13:13:32 -0800

    Stub for weasel words

---
+++ proselint/checks/inprogress/wgd_weasel_words.py
@@ -0,0 +1,23 @@
+"""WGD100: Weasel words.
+
+---
+layout:     post
+error_code: WGD200
+source:     write-good
+source_url: https://github.com/btford/write-good
+title:      WGD100&#58; Weasel words.
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+Weasel words clearly weaken various aspects of a number of your sentences.
+
+"""
+
+
+def check(text):
+
+    error_code = "WGD200"
+    msg = "Weasel words present."
+
+    return [(1, 1, error_code, msg)]
