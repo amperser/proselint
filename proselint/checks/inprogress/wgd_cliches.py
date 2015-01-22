d1fe50bb02ff0d0527b2e48713a8bd0651b519b4
Jordan Suchow <suchow@berkeley.edu>
2015-01-22 13:21:20 -0800

    Add stub for cliches

---
+++ proselint/checks/inprogress/wgd_cliches.py
@@ -0,0 +1,23 @@
+"""WGD101: Cliches.
+
+---
+layout:     post
+error_code: WGD101
+source:     write-good
+source_url: https://github.com/btford/write-good
+title:      WGD101&#58; Cliches
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+Cliches are cliche.
+
+"""
+
+
+def check(text):
+
+    error_code = "WGD101"
+    msg = "Cliche."
+
+    return [(1, 1, error_code, msg)]
