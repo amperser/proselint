f50f825eb2c24a128f43ebe9193a53b2b2cfb2e3
Jordan Suchow <suchow@berkeley.edu>
2015-01-28 01:45:29 -0800

    Add some supercession rules

---
+++ proselint/checks/mau_adducible.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""MAU106: adducible
+
+---
+layout:     post
+error_code: MAU105
+source:     Garner's Modern American Usage
+source_url: http://amzn.to/15wF76r
+title:      adducible
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+adducible. So spelled—not *adduceable. See -ABLE (A).
+
+"""
+from proselint.tools.supersede import supersede
+
+check = supersede("adducible", "adduceable", "MAU106")
