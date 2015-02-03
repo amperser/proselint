c1fcd0c389b8f1c3d4ad2e0fe6376ae34e27408f
Jordan Suchow <suchow@post.harvard.edu>
2015-02-02 20:34:10 -0800

    Add consistency checks

---
+++ proselint/checks/consistency/spelling.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""IEL100: Inconsistent spelling.
+
+---
+layout:     post
+error_code: IEL100
+source:     Intelligent Editing Ltd.
+source_url: http://bit.ly/1x3hYj7
+title:      Inconsistent spelling
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+Intelligent Editing Ltd. says:
+
+> Some words have more than one correct spelling. American, British, Australian
+and Canadian English all have their own preferences. Even within those, there
+can be multiple spellings. For example, in the UK 'realise' is often preferred.
+However, 'realize' has been used in British-English for centuries and is
+preferred in the Oxford English Dictionary. However, no matter which spelling
+is preferred, one thing is always wrong: you mustn't use two different
+spellings in the same document.
+"""
+from proselint.tools import consistency_check
+
+err = "IEL100"
+msg = "Inconsistent spelling of '{}' (vs. '{}')."
+
+word_pairs = [
+    ["advisor", "adviser"],
+    ["analyse", "analyze"],
+    ["centre", "center"],
+    ["emphasise", "emphasize"],
+    ["finalise", "finalize"],
+    ["focussed", "focused"],
+    ["labour", "labor"],
+    ["learnt", "learned"],
+    ["organise", "organize"],
+    ["organised", "organized"],
+    ["organising", "organizing"],
+    ["recognise", "recognize"],
+]
+
+check = consistency_check(word_pairs, err, msg)
