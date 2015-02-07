e4c77f790df2a94777e254b36534a930ea44ac3f
Jordan Suchow <suchow@post.harvard.edu>
2015-02-06 20:42:34 -0800

    Add password check

---
+++ proselint/checks/misc/password.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""MSC: Password in plain text.
+
+---
+layout:     post
+error_code: MSC
+source:     ???
+source_url: ???
+title:      password in plain text
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+Don't put pass
+"""
+from proselint.tools import blacklist, memoize
+
+err = "MSC103"
+msg = u"Don't put passwords in plain text."
+
+pwd_regex = "[:]* [\S]{6,30}"
+
+password = [
+    "the password is{}".format(pwd_regex),
+    "my password is{}".format(pwd_regex),
+    "the password's{}".format(pwd_regex),
+    "my password's{}".format(pwd_regex),
+]
+
+
+@memoize
+def check(text):
+    return blacklist(text, password, err, msg)
