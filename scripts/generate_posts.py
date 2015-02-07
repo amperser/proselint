0f9f395c8348366c2735935bb04091a44eacf93c
Jordan Suchow <suchow@berkeley.edu>
2015-02-07 09:56:20 -0800

    Fix up website autogenerator

--- scripts/generate_posts.py
+++ scripts/generate_posts.py
@@ -6,28 +6,30 @@ import re
 
 grandparent = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 checks_dir = os.path.join(grandparent, "proselint", "checks")
-listing = os.listdir(checks_dir)
+listing = os.walk(checks_dir)
 
 
 def is_check(fn):
-    return fn[-3:] == ".py" and not fn == "__init__.py"
+    return fn[-3:] == ".py" and (not fn[-11:] == "__init__.py") and (not "inprogress" in fn)
 
-for fn in listing:
-    if is_check(fn):
-        M = ast.parse(''.join(open(os.path.join(checks_dir, fn))))
-        docstring = ast.get_docstring(M)
-        error_code = re.search("error_code: (.*)\n", docstring).group(1)
-        head, sep, tail = docstring.partition("title: ")
-        docstring = head + sep + "     " + error_code + "&#58;" + tail[4:]
+for root, subdirs, files in listing:
+    for file in files:
+        fn = os.path.join(root, file)
+        if is_check(fn):
+            M = ast.parse(''.join(open(os.path.join(checks_dir, fn))))
+            docstring = ast.get_docstring(M)
+            error_code = re.search("error_code: (.*)\n", docstring).group(1)
+            head, sep, tail = docstring.partition("title: ")
+            docstring = head + sep + "     " + error_code + "&#58;" + tail[4:]
 
-        post_filename = os.path.join(
-            os.path.join(grandparent, "site", "_posts"),
-            str(datetime.date.today()) + "-" + docstring[0:6] + ".md")
+            post_filename = os.path.join(
+                os.path.join(grandparent, "site", "_posts"),
+                str(datetime.date.today()) + "-" + docstring[0:6] + ".md")
 
-        # Chop off the first two lines
-        for i in xrange(2):
-            docstring = '\n'.join(docstring.split('\n')[1:])
+            # Chop off the first two lines
+            for i in xrange(2):
+                docstring = '\n'.join(docstring.split('\n')[1:])
 
-        # Create a new post in the blog.
-        with open(post_filename, 'w') as f:
-            f.write(docstring)
+            # Create a new post in the blog.
+            with open(post_filename, 'w') as f:
+                f.write(docstring)
