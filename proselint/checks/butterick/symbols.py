65ed170b2d02b9fdf3bffba2958fa8a1b447e1dd
Jordan Suchow <suchow@post.harvard.edu>
2015-02-09 16:06:00 -0800

    Add examples from Butterick's Practical Typography

---
+++ proselint/checks/butterick/symbols.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""BTR100: Use the symbols.
+
+---
+layout:     post
+error_code: BTR100
+source:     Butterick's Practical Typography
+source_url: http://practicaltypography.com/
+title:      Tense present
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+Use the symbols.
+
+"""
+from proselint.tools import memoize
+import re
+
+
+@memoize
+def check(text):
+
+    err = "BTR100"
+
+    symbols = [
+        ["\s\(c\)\s", u"'{}' is a goofy alphabetic approximation. Use ©."],
+        ["\s\(TM\)\s", u"'{}' is a goofy alphabetic approximation. Use ™."],
+        ["\s\(R\)\s", u"'{}' is a goofy alphabetic approximation. Use ®."],
+        [u"Copy­right ©", u"'{}' is redundant. Use the word or the symbol."],
+        # [ur"...", "'...' is an approximation, use the ellipsis symbol '…'."]
+    ]
+
+    errors = []
+    for i in symbols:
+        for m in re.finditer(i[0], text, flags=re.UNICODE | re.IGNORECASE):
+            txt = m.group(0).strip()
+            errors.append((m.start(), m.end(), err, i[1].format(txt)))
+
+    return errors
