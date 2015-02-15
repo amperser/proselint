4fcd90ee78cfc1e1c8c9fd7d709f1c841e869bdf
Jordan Suchow <suchow@berkeley.edu>
2015-02-14 21:43:59 -0800

    Allow cross domain requests

--- app.py
+++ app.py
@@ -1,67 +1,23 @@
 """Web app that serves proselint's API."""
 
-from flask import Flask, request, make_response, current_app
+from flask import Flask, request, jsonify
 import subprocess
+from flask_cors import CORS, cross_origin
 import uuid
 import os
 import re
-import json
-from datetime import timedelta
-from functools import update_wrapper
 import urllib2
+import json
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = "Origin, X-Requested-With,Content-Type, Accept"
 
 
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    """Allow cross-domain requests."""
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        f.required_methods = ['OPTIONS']
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
-
-@app.route('/v1/', methods=["GET"])
-@crossdomain(
-    origin='*', headers=['Origin, X-Requested-With, Content-Type, Accept'])
+@app.route('/v1/')
+@cross_origin()  # allow all origins all methods.
 def lint():
-    """Run proselint on the provided text and return the results."""
+    """Run linter on the provided text and return the results."""
     id = uuid.uuid4()
     filename = os.path.join("tmp", "{}.md".format(id))
 
