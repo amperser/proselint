918dc3f7bca1bd926361eb1899f1e87e24641ee1
Jordan Suchow <suchow@berkeley.edu>
2015-02-26 18:19:52 -0800

    Add em- vs. en-, im- vs. in- check

--- proselint/checks/garner/preferred_forms.py
+++ proselint/checks/garner/preferred_forms.py
@@ -343,3 +343,66 @@ def check_able_atable(text):
     ]
 
     return preferred_forms_check(text, preferences, err, msg)
+
+
+@memoize
+def check_em_vs_em_and_en_vs_in(text):
+    """em- vs. en-, im- vs. in-."""
+
+    err = "MAU102"
+    msg = "'{}' is the preferred form."
+
+    preferences = [
+
+        ["embalm",      ["imbalm"]],
+        ["embark",      ["imbark"]],
+        ["embed",       ["imbed"]],
+        ["embitter",    ["imbitter"]],
+        ["emblaze",     ["imblaze"]],
+        ["embody",      ["imbody"]],
+        ["embolden",    ["imbolden"]],
+        ["embosom",     ["imbosom"]],
+        ["embower",     ["imbower"]],
+        ["embrown",     ["imbrown"]],
+        ["empanel",     ["impanel"]],
+        ["empower",     ["impower"]],
+        ["encage",      ["incage"]],
+        ["encapsulate", ["incapsulate"]],
+        ["encase",      ["incase"]],
+        ["enclasp",     ["inclasp"]],
+        ["encumber",    ["incumber"]],
+        ["encumbrance", ["incumbrance"]],
+        ["endow",       ["indow"]],
+        ["endowment",   ["indowment"]],
+        ["endue",       ["indue"]],
+        ["enfold",      ["infold"]],
+        ["engraft",     ["ingraft"]],
+        ["engulf",      ["ingulf"]],
+        ["enlace",      ["inlace"]],
+        ["enmesh",      ["inmesh"]],
+        ["ensheathe",   ["insheathe"]],
+        ["enshrine",    ["inshrine"]],
+        ["ensnare",     ["insnare"]],
+        ["ensoul",      ["insoul"]],
+        ["ensphere",    ["insphere"]],
+        ["enthrall",    ["inthrall"]],
+        ["enthrone",    ["inthrone"]],
+        ["entitle",     ["intitle"]],
+        ["entomb",      ["intomb"]],
+        ["entreat",     ["intreat"]],
+        ["entrench",    ["intrench"]],
+        ["entrust",     ["intrust"]],
+        ["entwine",     ["intwine"]],
+        ["entwist",     ["intwist"]],
+        ["enwind",      ["inwind"]],
+        ["enwrap",      ["inwrap"]],
+        ["enwreathe",   ["inwreathe"]],
+        ["imbrue",      ["embrue"]],
+        ["impale",      ["empale"]],
+        ["impoverish",  ["empoverish"]],
+        ["inflame",     ["enflame"]],
+        ["ingrain",     ["engrain"]],
+        ["inure",       ["enure"]],
+    ]
+
+    return preferred_forms_check(text, preferences, err, msg)
