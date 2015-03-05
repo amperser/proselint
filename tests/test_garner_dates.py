0ddcdeac0f4f58149c693726e97034c15aa1aeba
Lara A. Ross <lara@amperser.com>
2015-03-04 22:04:23 -0800

    Add framework for detecting topics

---
+++ tests/test_garner_dates.py
@@ -0,0 +1,23 @@
+from check import Check
+from proselint.checks.garner import dates
+from textblob import TextBlob
+
+
+class TestCheck(Check):
+
+    __test__ = True
+
+    def test_50s_hyphenation(self):
+        text = TextBlob(
+            """The 50's were swell."""
+        )
+        errors = dates.check_decade_apostrophes_short(text)
+        assert len(errors) == 1
+
+    def test_50_Cent_hyphenation(self):
+        text = TextBlob(
+            """Dr. Dre suggested to 50's manager that he look into signing
+            Eminem to the G-Unit record label."""
+        )
+        errors = dates.check_decade_apostrophes_short(text)
+        assert len(errors) == 0
---
+++ tests/test_topic_detector.py
@@ -0,0 +1,25 @@
+from proselint.tools import topics
+from textblob import TextBlob
+
+
+class TestTopicDetector(object):
+
+    def test_50_Cent_detector_on_topic(self):
+        """Check precision of the 50 Cent topic-detector."""
+        blob = TextBlob(
+            """With the aid of Eminem and Dr. Dre (who produced his first
+               major-label album, Get Rich or Die Tryin'), Jackson became one
+               of the world's best selling rappers and rose to prominence with
+               East Coast hip hop group G-Unit (which he leads de facto). """
+        )
+        assert("50 Cent" in topics(blob))
+
+        blob = TextBlob(
+            """Hip hop was started in the early 50's."""
+        )
+        assert("50 Cent" not in topics(blob))
+
+        blob = TextBlob(
+            """Nowadays it costs 50 cents to buy a lollipop."""
+        )
+        assert("50 Cent" not in topics(blob))
