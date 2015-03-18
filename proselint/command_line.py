29972b673af6b4999d03dd677a8848e285ce73a3
Jordan Suchow <suchow@berkeley.edu>
2015-03-17 23:48:16 -0700

    Remove debugging statements

--- proselint/command_line.py
+++ proselint/command_line.py
@@ -57,7 +57,6 @@ def lint(path):
         blob = textblob.TextBlob(f.read())
         errors = []
         for check in checks:
-            print check.__name__
             start = time.time()
             errors += check(blob)
 
@@ -141,8 +140,6 @@ def lintscore():
 def proselint(
         file=None, version=None, initialize=None, debug=None, score=None):
     """Define the linter command line API."""
-
-    print 'TEST'
 
     # Return the version number.
     if version:
