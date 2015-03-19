894c5a0417322329288b5c762a9a4e777c812f7e
Jordan Suchow <suchow@berkeley.edu>
2015-03-19 02:56:12 -0700

    Use web and worker processes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app:app --log-file=-
+worker: python worker.py
---
+++ worker.py
@@ -0,0 +1,15 @@
+import os
+
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
