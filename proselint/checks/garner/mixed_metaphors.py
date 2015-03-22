35a3589bbded91d2e8d9a43dd5ced2afdb44e5c1
Jordan Suchow <suchow@berkeley.edu>
2015-03-22 12:03:51 -0700

    Add stub for mixed metaphors

---
+++ proselint/checks/garner/mixed_metaphors.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""MAU104: Mixed metaphors.
+
+---
+layout:     post
+error_code: MAU104
+source:     Garner's Modern American Usage
+source_url: http://amzn.to/15wF76r
+title:      mixed metaphors
+date:       2014-06-10 12:31:19
+categories: writing
+---
+
+Archaism.
+
+"""
+from proselint.tools import preferred_forms_check, memoize
+
+
+@memoize
+def check(blob):
+    """Check the text."""
+    err = "MAU104"
+    msg = u"Mixed metaphor. Try '{}'."
+
+    preferences = [
+
+        ["cream rises to the top",   ["cream rises to the crop"]],
+        ["fasten your seatbelts",    ["button your seatbelts"]],
+    ]
+
+    return preferred_forms_check(blob, preferences, err, msg)
