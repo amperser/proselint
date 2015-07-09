a8daa5da6d7c6dda797006098719542607a4b936
Jordan Suchow <suchow@post.harvard.edu>
2015-07-09 00:58:41 -0700

    Remove last remnants of TextBlob

--- tests/test_garner_dates.py
+++ tests/test_garner_dates.py
@@ -1,6 +1,5 @@
 from check import Check
 from proselint.checks.garner import dates
-from textblob import TextBlob
 
 
 class TestCheck(Check):
@@ -8,16 +7,12 @@ class TestCheck(Check):
     __test__ = True
 
     def test_50s_hyphenation(self):
-        text = TextBlob(
-            """The 50's were swell."""
-        )
+        text = """The 50's were swell."""
         errors = dates.check_decade_apostrophes_short(text)
         assert len(errors) == 1
 
     def test_50_Cent_hyphenation(self):
-        text = TextBlob(
-            """Dr. Dre suggested to 50's manager that he look into signing
-            Eminem to the G-Unit record label."""
-        )
+        text = """Dr. Dre suggested to 50's manager that he look into signing
+                  Eminem to the G-Unit record label."""
         errors = dates.check_decade_apostrophes_short(text)
         assert len(errors) == 0
