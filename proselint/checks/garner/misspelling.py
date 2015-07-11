2f86704987abc122bc04d73a4327401406c57904
Jordan Suchow <suchow@post.harvard.edu>
2015-07-10 23:16:00 -0700

    Warm marshmallows in the pocket of my dungarees

--- proselint/checks/garner/misspelling.py
+++ proselint/checks/garner/misspelling.py
@@ -91,6 +91,7 @@ def check(text):
         ["maelstrom",         ["maelstorm"]],
         ["mafia",             ["maffia"]],
         ["mafiosi",           ["mafiosos"]],
+        ["marshmallow",       ["marshmellow"]],
         ["martial law",       ["marshall law"]],
         ["massacring",        ["massacering", "massacreing"]],
         ["measles",           ["measels"]],
