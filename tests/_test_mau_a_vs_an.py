2bb1e9be106b00851d76002fdc1f8b850a7cfeef
Jordan Suchow <suchow@post.harvard.edu>
2015-07-12 15:17:09 -0700

    Comply with PEP257

--- tests/_test_mau_a_vs_an.py
+++ tests/_test_mau_a_vs_an.py
@@ -6,13 +6,17 @@ from proselint.checks.garner import a_vs_an as chk
 
 class TestCheck(Check):
 
+    """Test garner.a_vs_n."""
+
     __test__ = True
 
     @property
     def this_check(self):
+        """Boilerplate."""
         return chk
 
     def test(self):
+        """Ensure the test works correctly."""
         assert self.check("""An apple a day keeps the doctor away.""")
         assert self.check("""The Epicurean garden.""")
         assert not self.check("""A apple a day keeps the doctor away.""")
