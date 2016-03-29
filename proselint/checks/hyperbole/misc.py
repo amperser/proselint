75f090d8869702f4bbf51bf7b6e332306a5ef2ab
Jordan Suchow <suchow@post.harvard.edu>
2016-03-29 14:17:58 -0700

    Reorganize into hyperbole module

--- proselint/checks/hyperbole/misc.py
+++ proselint/checks/hyperbole/misc.py
@@ -19,7 +19,7 @@ from proselint.tools import existence_check, memoize
 @memoize
 def check(text):
     """Check the text."""
-    err = "misc.hyperbolic"
+    err = "hyperbolic.misc"
     msg = u"'{}' is hyperbolic."
 
     words = [
