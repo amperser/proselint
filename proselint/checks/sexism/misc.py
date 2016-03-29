82ed9f578ddb431969e3361dea3d91bf76b238b9
Jordan Suchow <suchow@post.harvard.edu>
2016-03-29 14:38:39 -0700

    Reorganize into module on sexism

--- proselint/checks/sexism/misc.py
+++ proselint/checks/sexism/misc.py
@@ -19,7 +19,7 @@ from proselint.tools import memoize, preferred_forms_check
 @memoize
 def check(text):
     """Suggest the preferred forms."""
-    err = "garner.sexism"
+    err = "sexism.misc"
     msg = "Gender bias. Use '{}' instead of '{}'."
 
     sexism = [
