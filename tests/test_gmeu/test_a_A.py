1ad31df75ead5b56aa6b54fe8c89d28575adb1b5
Jordan Suchow <suchow@post.harvard.edu>
2016-04-05 16:19:27 -0700

    Add test for GMEU entry "a", part A

---
+++ tests/test_gmeu/test_a_A.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test GMEU entry 'a', part A."""
+
+from proselint.tools import assert_error
+from nose import SkipTest
+
+
+def test_a_vs_an():
+    """Use the correct determiner."""
+    raise SkipTest
+
+    sentences = [
+        "Are you an Yankee fan?",
+        "Coffee tastes less sweet in an white mug.",
+        "One of them wore a opalescent vest.",
+        "You're a intelligent guy, often misguided.",
+        "Ezra gave an eulogy.",
+        "What capital of an European country is the farthest north?",
+        "His sole reward was an one-year term as Ambassador to Thailand.",
+        "I will be relying on a roulette wheel and an Ouija board.",
+        "Anyone in an uniform is fair game.",
+        "Grimm started working as a F.B.I. agent in 1995.",
+        "Out of 186 managers participating, 57 had a MBA degree.",
+        "Smith announced that a SEC filing is pending.",
+        "This argument is an historical desecration.",
+        u"The treatment of crime in Britain shows an historic shift away…",
+        "It is, in some ways, an humble form.",
+        "The thief turned out to be an habitual offender from Darlington.",
+        "This stage displays an hallucinatory image that signifies itself.",
+        "He saw an hallucinatory image before passing out.",
+        "A triumphant Adolf Hitler addressed an hysterical crowd.",
+        "Kun pieced together an history of gender-segregated dining in L.A.",
+        "An historian who fled the Nazis and still wants us to read Hitler.",
+        "It is nominally an historical novel.",
+        "It feels good to validate an hypothesis.",
+        "An hereditary title can be passed to a member of the family.",
+        "The Clinton Presidency was an historic era of prosperity.",
+        "There comes to be an habitual pattern between neurons in the brain.",
+        "They had the authority to start an humanitarian intervention.",
+        "She laughted aloud, an hysterical sort of giggled, quickly stifled",
+    ]
+
+    error_check_name = "misc.a_vs_an"
+
+    for sentence in sentences:
+        yield assert_error, sentence, error_check_name
