1b971b32d57df48919e0c45372237d858996e3d9
Jordan Suchow <suchow@post.harvard.edu>
2016-04-05 23:06:20 -0700

    Add test for GMEU entry "a", part C

---
+++ tests/test_gmeu/test_a_C.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test GMEU entry 'a', part C.
+
+This entry is about pronunciation of the word 'a' and is unlikely to be
+relevant to usage in written language.
+"""
