2b87550fb0fed7e5a1076d97b67478ce0e6507c7
Joshua Grant <joshua.m.grant@gmail.com>
2016-07-17 22:52:08 -0400

    updated list for formatting

--- proselint/checks/glaad/terms.py
+++ proselint/checks/glaad/terms.py
@@ -28,12 +28,12 @@ def check(text):
         ["gay man",            ["homosexual man"]],
         ["gay men",            ["homosexual men"]],
         ["lesbian",            ["homosexual woman"]],
-        ["lesbians",            ["homosexual women"]],
-        ["gay people",            ["homosexual people"]],
-        ["gay couple",            ["homosexual couple"]],
-        ["sexual orientation",           ["sexual preference"]],
-        ["openly gay",    ["admitted homosexual", "avowed homosexual"]],
-        ["equal rights",            ["special rights"]]
+        ["lesbians",           ["homosexual women"]],
+        ["gay people",         ["homosexual people"]],
+        ["gay couple",         ["homosexual couple"]],
+        ["sexual orientation", ["sexual preference"]],
+        ["openly gay",         ["admitted homosexual", "avowed homosexual"]],
+        ["equal rights",       ["special rights"]]
         ]
 
     return preferred_forms_check(text, list, err, msg, ignore_case=False)
