c0cb926c836d0565661ae52b82231ddc44a4b1a5
Kyle Bedell <kyle.bedell@gmail.com>
2016-09-15 20:28:50 -0400

    Fix pep8 issues

--- proselint/checks/technical_language/javascript.py
+++ proselint/checks/technical_language/javascript.py
@@ -15,16 +15,18 @@ Points out technical expressions that are written incorrectly
 """
 from proselint.tools import memoize, preferred_forms_check, existence_check
 
+
 @memoize
 def check(text):
     """Suggest preferred forms."""
     err = "technical_language.javascript"
     msg = "Incorrect form. Use '{}' instead of '{}'."
 
-    technical_language = [
-        ["ECMAScript #",       ["ECMAScript([0-9]+)"]], #TODO
+    techlang = [
+        ["ECMAScript #",       ["ECMAScript([0-9]+)"]],  # TODO
         ["ES.next",             ["ES next"]],
-        # ["Web Components",      ["WebComponents", "web components", "Web components"]],
+        # ["Web Components",      \
+        # ["WebComponents", "web components", "Web components"]],
         ["localStorage",        ["localstorage"]],
         # ["sessionStorage",      ["session storage|sessionstorage"]],
         # ["HTML Imports",        ["HTML Import"]],
@@ -33,19 +35,19 @@ def check(text):
         # ["Touch Events",      ["touch event"]],
         ["Ember.js",            ["EmberJS"]]
     ]
-    
-    errors = preferred_forms_check(text, technical_language, err, msg, ignore_case=False)
-    
-    check_caps = [ 
-        "ES6 Classes", 
+
+    errors = preferred_forms_check(text, techlang, err, msg, ignore_case=False)
+
+    check_caps = [
+        "ES6 Classes",
         "JavaScript",
         "Web Audio API"
     ]
     check_caps_lower = [x.lower() for x in check_caps]
-    
+
     for m in existence_check(text, check_caps, err, "{}", ignore_case=True):
         original = m[3]
-        if original not in check_caps: 
+        if original not in check_caps:
             correct = check_caps[check_caps_lower.index(original.lower())]
             errors.append((
                 m[0],
