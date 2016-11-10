d2327022843ba983bfb33212643e5b65c3496e84
Jonathan Sanders <jonsandersss@gmail.com>
2016-11-10 17:30:20 -0500

    Update institution_name.py

--- proselint/checks/misc/institution_name.py
+++ proselint/checks/misc/institution_name.py
@@ -4,19 +4,18 @@
 
 from proselint.tools import memoize, preferred_forms_check
 
-
-
 @memoize
 def check_vtech(text):
     """Suggest the correct name.
 
     source: Virginia Tech Division of Student Affairs
-    source_url: http://career.vt.edu/job-search/presenting_yourself/resumes/common-resume-mistakes.html
+    source_url: http://bit.ly/2en1zbv
     """
     err = "institution.vtech"
     msg = "Incorrect name. Use '{}' instead of '{}'."
 
     institution = [
-        ["Virginia Polytechnic Institute and State University",          ["Virginia Polytechnic and State University"]],
+        ["Virginia Polytechnic Institute and State University",          
+         ["Virginia Polytechnic and State University"]],
     ]
     return preferred_forms_check(text, institution, err, msg)
