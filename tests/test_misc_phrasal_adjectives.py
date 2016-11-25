213d9296bc3f960f75c3e0d636aedf002d19654f
Jonathan Sanders <jonsandersss@gmail.com>
2016-11-25 15:47:40 -0500

    Coverage of issue 603

--- tests/test_misc_phrasal_adjectives.py
+++ tests/test_misc_phrasal_adjectives.py
@@ -25,3 +25,5 @@ class TestCheck(Check):
         """Basic smoke test for misc.phrasal_adjectives.check_ly."""
         assert chk.check_ly("""Smoke phrase with nothing flagged.""") == []
         assert chk.check_ly("""He ran swiftly-fast.""")
+        assert chk.check_ly("""The not-so-hotly-contested
+                             result was fine.""") == []
