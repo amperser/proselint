3e524b3b9969a5a63be725fec918b82562919dd5
Peter Oliver <git@mavit.org.uk>
2017-02-27 21:45:49 +0000

    Package flycheck-proselint.el.

--- plugins/flycheck/flycheck-proselint.el
+++ plugins/flycheck/flycheck-proselint.el
@@ -1,7 +1,23 @@
-; To install flycheck from MELPA, evaluate:
-;
-; (add-hook 'markdown-mode-hook #'flycheck-mode)
-; (add-hook 'text-mode-hook #'flycheck-mode)
+;;; flycheck-proselint.el --- a flycheck linter for prose
+
+;; Version: 0.8.0
+;; Package-Requires: ((flycheck "1"))
+;; Keywords: languages, convenience
+;; URL: http://proselint.com/
+
+;;; Commentary:
+
+;; proselint's goal is to aggregate knowledge about best practices in
+;; writing and to make that knowledge immediately accessible to all
+;; authors in the form of a linter for prose.
+;;
+;; This package provides a proselint checker for flycheck.
+;;
+;; To activate automatically, add to your .emacs, e.g.:
+;;   (add-hook 'markdown-mode-hook #'flycheck-mode)
+;;   (add-hook 'text-mode-hook #'flycheck-mode)
+
+;;; Code:
 
 (flycheck-define-checker proselint
   "A linter for prose."
@@ -13,4 +29,9 @@
                      (zero-or-more "\n" (any " ") (one-or-more not-newline)))
             line-end))
   :modes (text-mode markdown-mode gfm-mode))
+
 (add-to-list 'flycheck-checkers 'proselint)
+
+(provide 'flycheck-proselint)
+
+;;; flycheck-proselint.el ends here
