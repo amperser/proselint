a36cadf007e5dacf655c22b4966a6298d901c945
Eric Bailey <eric.w.bailey@gmail.com>
2017-10-14 19:27:03 -0400

    Added offensive terms

---
+++ proselint/checks/ableism/offensive_terms.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""Ableism.
+
+---
+layout:     post
+source:     Autistic Hoya: Ableism/Language
+source_url: http://www.autistichoya.com/p/ableist-words-and-terms-to-avoid.html
+title:      Ableism
+date:       2017-09-14
+categories: writing
+---
+
+This check looks for offensive terms related to ableism issues and
+raises an error marking them as offensive.
+
+"""
+from proselint.tools import memoize, existence_check
+
+
+@memoize
+def check(text):
+    """Flag offensive words based on Autistic Hoya's Ableism/Language list."""
+    err = "ableism.offensive_terms"
+    msg = "Offensive term. Remove it or consider the context."
+
+    list = [
+        "aspie",
+        "assburger",
+        "barren",
+        "crazy",
+        "cretin",
+        "crip",
+        "cripple",
+        "daft",
+        "dumb",
+        "flid",
+        "fucktard",
+        "freak",
+        "gimp",
+        "gimpy",
+        "hysteria",
+        "hysterical",
+        "idiot",
+        "idiotic",
+        "imbecile",
+        "insane",
+        "insanity",
+        "lame",
+        "libtard",
+        "loony",
+        "lunatic",
+        "maniac",
+        "midget",
+        "mongolism",
+        "mongoloid",
+        "moron",
+        "psycho",
+        "retard",
+        "retarded",
+        "tard",
+        "schizo",
+        "short bus",
+        "simpleton",
+        "spaz",
+        "spazzed",
+        "sperg",
+        "stupid",
+        "wacko",
+        "whacko"
+    ]
+    list = [term.replace(" ","[ -]") for term in list]
+
+    return existence_check(text, list, err, msg, join=True, ignore_case=False)
