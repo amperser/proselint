404b6dffb92e6e13d519144b8ba645788da55702
KVGarg <co16326.ccet@gmail.com>
2018-10-19 22:50:37 +0530

    Added checks for capitalizing the beginning of sentences
    1. Fixes Issue #800
    2. Added a rule( named split_into_sentences) in tools.py that splits the text into sentences and return all the sentences.
    3. Added check in misc.capitalization for checking the beginning of sentences.

--- proselint/checks/misc/capitalization.py
+++ proselint/checks/misc/capitalization.py
@@ -13,7 +13,10 @@ categories: writing
 Incorrect capitalization.
 
 """
-from proselint.tools import memoize, preferred_forms_check
+from proselint.tools import (memoize,
+                             preferred_forms_check,
+                             split_into_sentences,
+                             existence_check)
 
 
 @memoize
@@ -91,3 +94,18 @@ def check_days(text):
     ]
 
     return preferred_forms_check(text, list, err, msg, ignore_case=False)
+
+
+@memoize
+def check_beginning_of_sentences(text):
+    """Check for capitalizing the beginning of sentences."""
+    sentences = split_into_sentences(text)
+    err = "misc.capitalization"
+    msg = "Sentence `{}` should begin with capital letter."
+    sentences_tobe_capitalized = [_[:50] for _ in sentences if _[0].islower()]
+    if len(sentences_tobe_capitalized) > 0:
+        return existence_check(text,
+                               sentences_tobe_capitalized,
+                               err, msg, require_padding=False)
+    else:
+        return []
--- proselint/tools.py
+++ proselint/tools.py
@@ -4,16 +4,17 @@
 
 from __future__ import print_function
 from __future__ import unicode_literals
+
+import functools
+import hashlib
+import importlib
+import inspect
+import json
+import os
+import re
+import shelve
 import sys
 import traceback
-import os
-import shelve
-import inspect
-import functools
-import re
-import hashlib
-import json
-import importlib
 
 try:
     import dbm
@@ -30,6 +31,50 @@ _cache_shelves = dict()
 
 proselint_path = os.path.dirname(os.path.realpath(__file__))
 
+alphabets = "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = """
+            (Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|
+            Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"""
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.]([A-Za-z0-9]+)[.](com|net|org|io|gov)"
+digits = "([0-9])"
+
+
+def split_into_sentences(text):
+    """Split the text into sentences."""
+    text = " " + text + "  "
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, "\\1<prd>", text)
+    text = re.sub(websites, "<prd>\\1<prd>\\2", text)
+    text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]",
+                  "\\1<prd>\\2<prd>\\3<prd>", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]",
+                  "\\1<prd>\\2<prd>", text)
+    text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
+    text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text:
+        text = text.replace(".”", "”.")
+    if "\"" in text:
+        text = text.replace(".\"", "\".")
+    if "!" in text:
+        text = text.replace("!\"", "\"!")
+    if "?" in text:
+        text = text.replace("?\"", "\"?")
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
 
 def close_cache_shelves():
     """Close previously opened cache shelves."""
@@ -44,6 +89,7 @@ def close_cache_shelves_after(f):
     def wrapped(*args, **kwargs):
         f(*args, **kwargs)
         close_cache_shelves()
+
     return wrapped
 
 
@@ -261,7 +307,7 @@ def lint(input_file, debug=False):
             (line, column) = line_and_column(text, start)
             if not is_quoted(start, text):
                 errors += [(check, message, line, column, start, end,
-                           end - start, "warning", replacements)]
+                            end - start, "warning", replacements)]
 
         if len(errors) > options["max_errors"]:
             break
@@ -275,7 +321,7 @@ def lint(input_file, debug=False):
 def assert_error(text, check, n=1):
     """Assert that text has n errors of type check."""
     assert_error.description = "No {} error for '{}'".format(check, text)
-    assert(check in [error[0] for error in lint(text)])
+    assert (check in [error[0] for error in lint(text)])
 
 
 def consistency_check(text, word_pairs, err, msg, offset=0):
@@ -426,7 +472,7 @@ def is_quoted(position, text):
                 s = 2
             elif s == 2:
                 if c in seps:
-                    ranges.append((start+1, i-1))
+                    ranges.append((start + 1, i - 1))
                     start = None
                     s = 0
                 else:
