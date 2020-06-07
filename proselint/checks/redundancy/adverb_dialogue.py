10f71f783f059ad1981899dfcd3bd7cb226fa589
serene-arc <serenical@gmail.com>
2020-06-07 20:05:01 +1000

    Add basic checking for adverbs after dialogue

---
+++ proselint/checks/redundancy/adverb_dialogue.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Redundancy."""
+
+from proselint.tools import memoize, is_quoted
+import re
+
+@memoize
+def check(text):
+    """Suggest the preferred forms"""
+    err = "redundancy.adverbdialogue"
+    msg = "Redundant adverb. Remove {}."
+
+    # find if any of the text is in quote
+    regex = '[\'"].*[,?!][\'"][\s\w]*?(\w+?ly)[\s\w]*[,.!?]'
+    matches = re.finditer(regex,text)
+    return [(match.start(1), match.end(1), err, msg.format(match.groups(0)[0]), None) for match in matches]
