47a872b48b06781e6f5e29df7dab40d19b875a4b
serene-arc <serenical@gmail.com>
2020-06-08 10:14:17 +1000

    Implement linting changes

--- proselint/checks/redundancy/adverb_dialogue.py
+++ proselint/checks/redundancy/adverb_dialogue.py
@@ -5,13 +5,15 @@
 from proselint.tools import memoize, is_quoted
 import re
 
+
 @memoize
 def check(text):
-    """Suggest the preferred forms"""
+    """Suggest the preferred forms."""
     err = "redundancy.adverbdialogue"
     msg = "Redundant adverb. Remove {}."
 
     # find if any of the text is in quote
-    regex = '[\'"].*[,?!][\'"][\s\w]*?(\w+?ly)[\s\w]*[,.!?]'
-    matches = re.finditer(regex,text)
-    return [(match.start(1), match.end(1), err, msg.format(match.groups(0)[0]), None) for match in matches]
+    regex = r'[\'"].*[,?!][\'"][\s\w]*?(\w+?ly)[\s\w]*[,.!?]'
+    matches = re.finditer(regex, text)
+    return [(match.start(1), match.end(1), err, msg.format(
+        match.groups(0)[0]), None) for match in matches]
