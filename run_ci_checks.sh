dceb446594ea8c99883ad3f064b5dcc59691535e
Nytelife26 <xtylerjrx@gmail.com>
2021-04-22 21:59:28 +0100

    chore: bootstrap poetry

--- run_ci_checks.sh
+++ run_ci_checks.sh
@@ -1,28 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# This script runs the same commands that are run for each pull request before it
-# can be merged.
-#
-# Run this script before you open a pull request to make sure your changes are good.
+# This script runs the same commands that are run for each pull request
+# before it can be merged. Run this script before you open a pull request
+# to make sure your changes are good.
 
-if ! which pep257 >/dev/null; then
-    echo -e "pep257 is not installed. Please install it with 'pip install pep257'."
+if ! which poetry >/dev/null; then
+    echo -e "
+poetry is not set up. It is required as our dependency sandbox for proselint.
+Please install it from python-poetry.org."
     exit 1
 fi
 
-if ! pep257 . --match='.*\.py'; then
-    echo -e "\n\nPEP-257 issues found. Please fix them and re-run this script."
+if ! poetry run pycodestyle --config pyproject.toml || ! poetry run pydocstyle; then
+    echo -e "\n\n=== LINT ISSUES FOUND! SEE ABOVE. ==="
     exit 1
 fi
 
-if ! which nosetests >/dev/null; then
-    echo -e "nose is not installed. Please install it with 'pip install nose'."
+if ! poetry run pytest --cov=proselint --continue-on-collection-errors; then
+    echo -e "\n\n=== TESTS FAILED! SEE ABOVE. ==="
     exit 1
 fi
 
-if ! nosetests --with-coverage --cover-package proselint; then
-    echo -e "\n\nSome tests failed. Please fix them and re-run this script."
-    exit 1
-fi
-
-echo -e "\n\nSuccess! Your pull request looks good to submit."
+echo -e "\n\nSuccess! Your pull request is ready to submit."
