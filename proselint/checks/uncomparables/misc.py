61a9973b13912640c9c6fe9caa035c1827f1186d
Nytelife26 <xtylerjrx@gmail.com>
2021-05-17 03:31:10 +0100

    fix: reduce false positives in uncomparables

--- proselint/checks/uncomparables/misc.py
+++ proselint/checks/uncomparables/misc.py
@@ -114,7 +114,7 @@ def check(text):
         ("more", "possible")  # FIXME
     ]
 
-    all = [i[0] + r"\s" + i[1] + r"[\W$]" for i in itertools.product(
+    all = [fr"(^|\W){i[0]}\s{i[1]}[\W$]" for i in itertools.product(
            comparators, uncomparables) if i not in exceptions]
 
     occ = re.finditer("|".join(all), text.lower())
