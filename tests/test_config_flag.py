93117cc024b8025e99323f8447b0a9cd62eb4d15
Nytelife26 <xtylerjrx@gmail.com>
2021-05-24 10:18:15 +0100

    test(config): use load_options instead of subprocess

--- tests/test_config_flag.py
+++ tests/test_config_flag.py
@@ -1,20 +1,9 @@
 """Check that the --config command-line flag works"""
 
-import subprocess
+from proselint.tools import load_options
 
 
-def test_default_config():
-    """Ensure that linting the demo uses the default proselintrc with
-    uncomparables.misc true"""
-    output = subprocess.run(["python", "-m", "proselint", "--demo"],
-                            stdout=subprocess.PIPE, encoding='utf-8')
-    assert "uncomparables.misc" in output.stdout
-
-
-def test_overriding_config():
-    """Ensure that linting the demo and overriding uncomparables.misc to false
-    has no such warnings"""
-    output = subprocess.run(["python", "-m", "proselint", "--demo", "--config",
-                             "tests/test_config_flag_proselintrc"],
-                            stdout=subprocess.PIPE, encoding='utf-8')
-    assert "uncomparables.misc" not in output.stdout
+def test_load_options_function():
+    overrides = load_options("tests/test_config_flag_proselintrc")
+    assert load_options()["checks"]["uncomparables.misc"]
+    assert not overrides["checks"]["uncomparables.misc"]
