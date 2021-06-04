44e990dd3522808d54e8c7998cb05f1bbc699a08
Nytelife26 <xtylerjrx@gmail.com>
2021-06-04 19:59:07 +0100

    refactor: divide misc psychology check

--- proselint/checks/psychology/lie_detector.py
+++ proselint/checks/psychology/lie_detector.py
@@ -17,7 +17,7 @@ from proselint.tools import preferred_forms_check, existence_check, memoize
 
 
 @memoize
-def check_lie_detector_test(text):
+def check(text):
     """Suggest the preferred forms."""
     err = "psychology.lie_detector"
     msg = "Polygraph machines measure arousal, not lying per se. Try {}."
@@ -30,25 +30,3 @@ def check_lie_detector_test(text):
     return preferred_forms_check(text, list, err, msg)
 
 
-@memoize
-def check_p_equals_zero(text):
-    """Check for p = 0.000."""
-    err = "psychology.p_equals_zero"
-    msg = "Unless p really equals zero, you should use more decimal places."
-
-    list = [
-        "p = 0.00",
-        "p = 0.000",
-        "p = 0.0000",
-    ]
-
-    return existence_check(text, list, err, msg, join=True)
-
-
-@memoize
-def check_mental_telepathy(text):
-    """Check for 'mental telepathy'."""
-    err = "psychology.mental_telepathy"
-    msg = "This is redundant because all purported telepathy is mental."
-
-    return existence_check(text, ["mental telepathy"], err, msg)
