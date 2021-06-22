f5948b7d996780c854b5a3aa5f5450a501dc4ca3
joshgrant <joshua.m.grant@gmail.com>
2021-06-21 23:36:46 -0400

    test: skip memoization in test runs

--- tests/check.py
+++ tests/check.py
@@ -31,7 +31,7 @@ class Check(TestCase):
 
         errors = []
         for text in lst:
-            errors.append(self.this_check.check(text))
+            errors.append(self.this_check.check.__wrapped__(text))
 
         return len(errors[0]) == 0
 
@@ -51,7 +51,7 @@ class Check(TestCase):
             # Compute the number of words per (wpe) error.
             with codecs.open(example_path, "r", encoding='utf-8') as f:
                 text = f.read()
-                num_errors = len(self.this_check.check(text))
+                num_errors = len(self.this_check.check.__wrapped__(text))
                 num_words = len(text)
 
             try:
