a13cc04e1bad507a74a2607a4364c97844bb34f2
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2024-01-02 18:26:35 +0100

    Update test_cache_function.py

--- tests/test_cache_function.py
+++ tests/test_cache_function.py
@@ -6,19 +6,37 @@ from proselint import lint_cache
 
 def test_speed():
     """compare uncached vs cached reads"""
+    import proselint
+
     demo_path = proselint.path / "demo.md"
-    demo_fh = demo_path.open(encoding="utf-8", errors="replace")
-    _code_a = """
-    lint_cache.cache.clear()
-    errors = proselint.tools.lint(demo_fh)
-    """
-    _code_b = """errors = proselint.tools.lint(demo_fh)"""
+    _code = "errors = proselint.tools.lint(demo_fh)"
+    repetitions = 2
 
+    # make sure it works
     lint_cache.cache.clear()
+    with demo_path.open(encoding="utf-8", errors="replace") as demo_fh:
+        errors = proselint.tools.lint(demo_fh)
+    assert len(errors) > 0
 
-    _dur1 = timeit(_code_a, globals=globals(), number=4)
-    _dur2 = timeit(_code_b, globals=globals(), number=4)
-    _dur3 = timeit(_code_a, globals=globals(), number=4)
+    # without cache
+    _dur1 = 0.0
+    for _ in range(repetitions):
+        with demo_path.open(encoding="utf-8", errors="replace") as demo_fh:
+            lint_cache.cache.clear()
+            _dur1 += timeit(_code, globals=locals(), number=1)
+
+    # with cache
+    _dur2 = 0.0
+    for _ in range(repetitions):
+        with demo_path.open(encoding="utf-8", errors="replace") as demo_fh:
+            _dur2 += timeit(_code, globals=locals(), number=1)
+
+    # without cache, confirmation
+    _dur3 = 0.0
+    for _ in range(repetitions):
+        with demo_path.open(encoding="utf-8", errors="replace") as demo_fh:
+            lint_cache.cache.clear()
+            _dur3 += timeit(_code, globals=locals(), number=1)
 
     assert _dur2 < 0.7 * _dur1
     assert _dur2 < 0.7 * _dur3
@@ -28,8 +46,9 @@ def test_consistency():
     """compare result-count"""
     demo_path = proselint.path / "demo.md"
     demo_fh = demo_path.open(encoding="utf-8", errors="replace")
+    demo_str = demo_fh.read()
     lint_cache.cache.clear()
-    errors_a = proselint.tools.lint(demo_fh)
-    errors_b = proselint.tools.lint(demo_fh)
+    errors_a = proselint.tools.lint(demo_str)
+    errors_b = proselint.tools.lint(demo_str)
 
     assert len(errors_a) == len(errors_b)
