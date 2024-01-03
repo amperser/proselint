a3b78a16f5ee2539f7fddf39f75bbfab145996ff
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2024-01-03 19:16:29 +0100

    extend benchmark scripts

--- scripts/benchmark_linter.py
+++ scripts/benchmark_linter.py
@@ -3,7 +3,6 @@ from multiprocessing import freeze_support
 freeze_support()
 
 import hashlib
-import time
 from timeit import timeit
 
 import proselint
@@ -19,9 +18,9 @@ file_path = proselint.path / "demo.md"
 _cfg = proselint.config_default
 options = {
     "ser_ser": [False, False],
-#    "ser_PAR": [False, True],
-#    "PAR_ser": [True, False],
-#    "PAR_PAR": [True, True],
+    #    "ser_PAR": [False, True],
+    #    "PAR_ser": [True, False],
+    #    "PAR_PAR": [True, True],
 }
 
 with file_path.open(encoding="utf-8", errors="replace") as fh:
@@ -33,10 +32,22 @@ _hash = hashlib.sha224(_text.encode("utf-8")).hexdigest()
 for _name, _val in options.items():
     _cfg["parallelize_lints"], _cfg["parallelize_checks"] = _val
     lint_cache.cache.clear()
-    t1 = timeit("e1 = proselint.tools.lint(_text, _cfg, _checks, _hash)", globals=globals(), number=1)
+    t1 = timeit(
+        "e1 = proselint.tools.lint(_text, _cfg, _checks, _hash)",
+        globals=globals(),
+        number=1,
+    )
     print(f"Lint with cfg={_name} took {t1 * 1e3:4.3f} ms uncached")
     lint_cache.cache.clear()
-    t2 = timeit("e1 = proselint.tools.lint(_text, _cfg, _checks, _hash)", globals=globals(), number=1)
+    t2 = timeit(
+        "e1 = proselint.tools.lint(_text, _cfg, _checks, _hash)",
+        globals=globals(),
+        number=1,
+    )
     print(f"Lint with cfg={_name} took {t2 * 1e3:4.3f} ms uncached rerun")
-    t3 = timeit("e2 = proselint.tools.lint(_text, _cfg, _checks, _hash)", globals=globals(), number=1)
+    t3 = timeit(
+        "e2 = proselint.tools.lint(_text, _cfg, _checks, _hash)",
+        globals=globals(),
+        number=1,
+    )
     print(f"Lint with cfg={_name} took {t3 * 1e3:4.3f} ms cached")
--- scripts/profile_linter.py
+++ scripts/profile_linter.py
@@ -1,5 +1,4 @@
 import cProfile
-from pathlib import Path
 
 import proselint
 from proselint import lint_cache
