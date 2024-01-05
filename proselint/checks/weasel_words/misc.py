94956d5a484967d0f6e49d8132d1f6188b88be5f
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2024-01-05 19:15:15 +0100

    add weasel words

--- proselint/checks/weasel_words/misc.py
+++ proselint/checks/weasel_words/misc.py
@@ -16,12 +16,31 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from proselint.checks import existence_check
+
 if TYPE_CHECKING:
     from proselint.checks import ResultCheck
 
 
-def disabled_check(text: str) -> list[ResultCheck]:
-    error_code = "weasel_words.misc"  # noqa: F841
-    msg = "Weasel words present."  # noqa: F841
+def check(text: str) -> list[ResultCheck]:
+    """
+    source: https://en.wikipedia.org/wiki/Weasel_word
+    """
+    error_code = "weasel_words.misc"
+    msg = "Weasel words aka. anonymous authority present ({})."
 
-    return []  # TODO
+    items = [
+        # vague
+        "evidence suggests",
+        "in most respects",
+        "some people",
+        "somewhat",
+        # false authority
+        "it has been decided" "it is said",
+        "it is thought",
+        "mistakes were made",
+        "researchers believe",
+        "some people say",
+    ]
+
+    return existence_check(text, items, error_code, msg)
