13be8b9d29583ea4f796794c88cae50f6121de91
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2024-01-14 17:31:14 +0100

    add more scientific checks: emotion, weasel words, conversation, preferred terms

--- proselint/checks/scientific/misc.py
+++ proselint/checks/scientific/misc.py
@@ -26,3 +26,42 @@ def check_num_unit(text: str) -> list[ResultCheck]:
     msg = "In scientific writings there is a whitespace between number and its unit, here '{}'."
     regex = Pd.sep_in_txt.value.format("(\d+(k|M|G|T|E|m|u|n|p|f)?(s|m|g|A|K|mol|cd|n))")
     return simple_existence_check(text, regex, err, msg, ignore_case=False)
+
+def check_emotion(text: str) -> list[ResultCheck]:
+    """avoid the following words"""
+    # src = https://www.sciencewrites.org/dos-and-donts
+    err = "scientific.misc.emotion"
+    msg = "Scientific writing should not contain emotionally laden terms like '{}'"
+    items = ["ridiculous", "unfortunately"]
+    return existence_check(text, items, err, msg)
+
+
+def check_weasel(text: str) -> list[ResultCheck]:
+    """avoid the following words"""
+    # src = https://www.sciencewrites.org/dos-and-donts
+    err = "scientific.misc.weasel"
+    msg = "Scientific writing should not build on uncertainty like '{}'"
+    items = ["attempts"]
+    return existence_check(text, items, err, msg)
+
+
+def check_conversation(text: str) -> list[ResultCheck]:
+    """avoid the following words"""
+    # src = https://www2.spsc.tugraz.at/www-archive/downloads/DoesandDonts%20in%20scientific%20writing%20reisenhofer%20tumfart.pdf
+    err = "scientific.misc.conversation"
+    msg = "Scientific writing should not chatter / converse like '{}'"
+    items = ["Well", "you see", "yes", "let's move on"]
+    return existence_check(text, items, err, msg)
+
+def check_preferred(text: str) -> list[ResultCheck]:
+    # src = https://www.sciencewrites.org/dos-and-donts
+    err = "scientific.misc.preference"
+    msg = "In scientific writing some terms are less preferred like '{}' - use '{}' instead"
+    items = [
+        ["not significant", ["insignificant"]],
+        ["although", ["though"]],
+        ["they are", ["they're"]],
+        ["is not", ["isn't"]],
+        ["cannot", ["can't"]],
+    ]
+    return existence_check(text, items, err, msg)
