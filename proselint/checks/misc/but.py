6ceaf6a7c0282f91c6aebd9c7566dd183688bed0
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2024-01-20 11:05:08 +0100

    Update but.py

--- proselint/checks/misc/but.py
+++ proselint/checks/misc/but.py
@@ -22,11 +22,12 @@ examples_pass = [
     "Smoke phrase with nothing flagged.",
     """I never start with the word "but",
 but might use it after a linebreak.""",
+    "Butter is the best.",
 ]
 
 examples_fail = [
     'But I never start with the word "but".',
-    'But why are you like that.',
+    "But why are you like that.",
     "This is cool! But that not so much.",
     "Is this cool? But that not so much.",
 ]
@@ -36,8 +37,8 @@ def check(text: str) -> list[ResultCheck]:
     """Do not start a paragraph with a 'But'."""
     err = "misc.but"
     msg = "No paragraph or sentence should start with a 'But'."
-    #regex = r"(^|([\n\r\.]+))(\s*)But"
-    regex = r"\b(^|[\.!\?]\s*)But"  # more powerful & 50% less computation
+    # regex = r"(^|([\n\r\.]+))(\s*)But"
+    regex = r"\b(?:^|[\.!\?]\s*)But\b"  # more powerful & 50% less computation
 
     return existence_check(
         text, [regex], err, msg, ignore_case=False, padding=Pd.disabled
