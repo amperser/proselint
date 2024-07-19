c96b9907edd54f873e1220dfe950a3aeebbaea2d
Nytelife26 <xtylerjrx@gmail.com>
2024-07-19 23:12:38 +0100

    fix: correct issue in dates_times index registry

--- proselint/checks/dates_times/__init__.py
+++ proselint/checks/dates_times/__init__.py
@@ -1,7 +1,7 @@
 """Dates and times."""
 
 from proselint.checks.dates_times.am_pm import __register__ as register_am_pm
-from proselint.checks.dates_times.am_pm import __register__ as register_dates
+from proselint.checks.dates_times.dates import __register__ as register_dates
 
 __register__ = (
     *register_am_pm,
