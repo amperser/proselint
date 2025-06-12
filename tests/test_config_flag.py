dc51b7cb86fe1abbd2ed0cba717a200d38236325
Nytelife26 <xtylerjrx@gmail.com>
2025-06-12 20:24:22 +0100

    test: update config and tools tests

--- tests/test_config_flag.py
+++ tests/test_config_flag.py
@@ -1,6 +1,5 @@
 """Test user option overrides using --config and load_options"""
 import json
-import os
 from pathlib import Path
 from unittest import TestCase
 from unittest.mock import patch
@@ -8,12 +7,11 @@ from unittest.mock import patch
 from click.testing import CliRunner
 
 from proselint.command_line import proselint
-from proselint.config import default
-from proselint.tools import deepmerge_dicts, load_options
+from proselint.config import DEFAULT, _deepmerge_dicts, load_from
 
 runner = CliRunner()
 
-CONFIG_FILE = str(Path(__file__, "../test-proselintrc.json").resolve())
+CONFIG_FILE = Path(__file__).parent / "test-proselintrc.json"
 FLAG = f"--config '{CONFIG_FILE}'"
 
 
@@ -21,22 +19,16 @@ def test_deepmerge_dicts():
     """Test deepmerge_dicts"""
     d1 = {'a': 1, 'b': {'c': 2, 'd': 3}}
     d2 = {'a': 2, 'b': {'c': 3, 'e': 4}}
-    assert deepmerge_dicts(d1, d2) == {'a': 2, 'b': {'c': 3, 'd': 3, 'e': 4}}
+    assert _deepmerge_dicts(d1, d2) == {'a': 2, 'b': {'c': 3, 'd': 3, 'e': 4}}
 
-
-@patch("os.path.isfile")
-def test_load_options_function(isfile):
+def test_load_from():
     """Test load_options by specifying a user options path"""
 
-    isfile.side_effect = CONFIG_FILE.__eq__
-
-    overrides = load_options(CONFIG_FILE, default)
-    assert load_options(conf_default=default)["checks"]["uncomparables"]
+    overrides = load_from(CONFIG_FILE)
+    assert load_from()["checks"]["uncomparables"]
     assert not overrides["checks"]["uncomparables"]
 
-    isfile.side_effect = os.path.join(os.getcwd(), ".proselintrc.json").__eq__
-
-    TestCase().assertRaises(FileNotFoundError, load_options)
+    TestCase().assertRaises(FileNotFoundError, lambda: load_from(Path("NULL")))
 
 
 def test_config_flag():
@@ -59,7 +51,7 @@ def test_config_flag():
 def test_dump_config():
     """Test --dump-default-config and --dump-config"""
     output = runner.invoke(proselint, "--dump-default-config")
-    assert json.loads(output.stdout) == default
+    assert json.loads(output.stdout) == DEFAULT
 
     output = runner.invoke(proselint, f"--dump-config {FLAG}")
-    assert json.loads(output.stdout) == json.load(open(CONFIG_FILE))
+    assert json.loads(output.stdout) == json.load(CONFIG_FILE.open())
