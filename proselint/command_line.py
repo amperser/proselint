5abe9a7ee19ad22c973f4c0d5bb5e837dde9e892
Nytelife26 <xtylerjrx@gmail.com>
2025-08-24 02:10:47 +0100

    refactor(cli): switch click -> argparse
    This improves startup overhead and dependency sizes by removing the need
    for `click`, achieving equivalent functionality with Python's built-in
    `argparse`.
    
    It also refactors the CLI into subcommands (check, dump-config).

--- proselint/command_line.py
+++ proselint/command_line.py
@@ -2,59 +2,64 @@
 
 import json
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
-
-import click
 
 from proselint.checks import __register__
 from proselint.config import DEFAULT, load_from
 from proselint.config import paths as config_paths
 from proselint.registry import CheckRegistry
-from proselint.tools import LintFile, extract_files
+from proselint.tools import LintFile, OutputFormat, extract_files
 from proselint.version import __version__
 
-CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
 base_url = "proselint.com/"
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.version_option(__version__, "--version", "-v", message="%(version)s")
-@click.option(
-    "--config",
-    is_flag=False,
-    type=click.Path(exists=True, path_type=Path),
-    help="Path to configuration file.",
-)
-@click.option("--debug", "-d", is_flag=True, help="Give verbose output.")
-@click.option(
-    "--json", "-j", "output_json", is_flag=True, help="Output as JSON."
-)
-@click.option("--demo", is_flag=True, help="Run over demo file.")
-@click.option("--compact", is_flag=True, help="Shorten output.")
-@click.option("--dump-config", is_flag=True, help="Prints current config.")
-@click.option(
-    "--dump-default-config", is_flag=True, help="Prints default config."
-)
-@click.argument("paths", nargs=-1, type=click.Path(exists=True, path_type=Path))
-def proselint(
-    paths: list[Path],
-    config=None,
-    version=None,
-    debug=None,
-    output_json=None,
-    demo=None,
-    compact=None,
-    dump_config=None,
-    dump_default_config=None,
-) -> None:
+def proselint() -> None:
     """Create the CLI for proselint, a linter for prose."""
-    if dump_default_config:
-        return print(json.dumps(DEFAULT, sort_keys=True, indent=4))
+    global_parser = ArgumentParser()
+    _ = global_parser.add_argument("--config", type=Path)
+    _ = global_parser.add_argument(
+        "--output-format",
+        "-o",
+        choices=[output_format.value for output_format in OutputFormat],
+        default=OutputFormat.FULL,
+        type=OutputFormat,
+    )
+    _ = global_parser.add_argument("--verbose", "-v", action="store_true")
+    _ = global_parser.add_argument("--version", "-V", action="store_true")
 
-    config = load_from(config)
-    if dump_config:
-        print(json.dumps(config, sort_keys=True, indent=4))
-        return None
+    parser = ArgumentParser(
+        parents=(global_parser,), conflict_handler="resolve"
+    )
+    subparsers = parser.add_subparsers(dest="subcommand")
+
+    check_parser = subparsers.add_parser(
+        "check", parents=(global_parser,), conflict_handler="resolve"
+    )
+    _ = check_parser.add_argument("--demo", action="store_true")
+    _ = check_parser.add_argument("paths", nargs="*", type=Path)
+
+    dump_config_parser = subparsers.add_parser(
+        "dump-config", parents=(global_parser,), conflict_handler="resolve"
+    )
+    _ = dump_config_parser.add_argument("--default", action="store_true")
+
+    args = parser.parse_args()
+
+    config = load_from(args.config)
+
+    if args.version:
+        return print(f"Proselint {__version__}\nPython {sys.version}")
+
+    if args.subcommand is None:
+        return parser.print_help()
+    if args.subcommand == "dump-config":
+        return print(
+            json.dumps(
+                DEFAULT if args.default else config, sort_keys=True, indent=4
+            )
+        )
 
     # Lint the files
     num_errors = 0
@@ -62,19 +67,17 @@ def proselint(
     CheckRegistry().register_many(__register__)
 
     lint_files: list[LintFile] = []
-    if demo:
+    if args.demo:
         lint_files = [LintFile(config_paths.demo_path)]
-    elif len(paths) == 0:
+    elif len(args.paths) == 0:
         lint_files = [LintFile.from_stdin()]
     else:
-        lint_files = list(map(LintFile, extract_files(paths)))
+        lint_files = list(map(LintFile, extract_files(args.paths)))
 
     for lint_file in lint_files:
         results = lint_file.lint(config)
         num_errors += len(results)
-        lint_file.output_errors(
-            results, output_json=output_json, compact=compact
-        )
+        lint_file.output_errors(results, args.output_format)
     # Return an exit code
     sys.exit(int(num_errors > 0))
 
