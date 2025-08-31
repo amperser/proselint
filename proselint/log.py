f196986bbaea3aa7f6bc467581fd95da0b61c011
Nytelife26 <xtylerjrx@gmail.com>
2025-08-31 12:49:21 +0100

    refactor(cli): use a level clamp for log filtering
    Co-authored-by: drainpixie <121581793+drainpixie@users.noreply.github.com>

--- proselint/log.py
+++ proselint/log.py
@@ -7,22 +7,24 @@ from sys import stderr, stdout
 from typing import Callable, TextIO
 
 
-def _get_level_filter(
-    level: int, *, invert: bool = False,
+def _level_clamp_filter(
+    *, min_level: int = logging.NOTSET, max_level: int = logging.CRITICAL,
 ) -> Callable[[logging.LogRecord], bool]:
-    """Create a filter function that allows log records of a given `level`."""
+    """Create a filter that allows log levels in the given closed interval."""
     def _filter(record: logging.LogRecord) -> bool:
-        return record.levelno >= level if invert else record.levelno <= level
+        return min_level <= record.levelno <= max_level
     return _filter
 
 
 def _init_stream_handler(
-    stream: TextIO, level: int, *, invert: bool = False
+    stream: TextIO,
+    level: int,
+    log_filter: Callable[[logging.LogRecord], bool],
 ) -> logging.StreamHandler[TextIO]:
     """Initialise a `StreamHandler` for logging with a level filter."""
     handler = logging.StreamHandler(stream)
     handler.setLevel(level)
-    handler.addFilter(_get_level_filter(level, invert=invert))
+    handler.addFilter(log_filter)
     return handler
 
 
@@ -33,9 +35,15 @@ class Logger(logging.Logger):
     def setup(*, verbose: bool = False) -> None:
         """Initialise the logger."""
         base_handler = _init_stream_handler(
-            stdout, logging.DEBUG if verbose else logging.INFO
+            stdout,
+            logging.DEBUG if verbose else logging.INFO,
+            _level_clamp_filter(max_level=logging.INFO)
         )
-        err_handler = _init_stream_handler(stderr, logging.WARNING, invert=True)
+        err_handler = _init_stream_handler(
+            stderr,
+            logging.WARNING,
+            _level_clamp_filter(min_level=logging.WARNING)
+        )
 
         logging.basicConfig(
             level=logging.NOTSET,
