06685524dd9103b4526b096642b80d614f75a617
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-09-10 00:01:03 +0200

    feat: implement custom check

--- proselint/checks/misc/but.py
+++ proselint/checks/misc/but.py
@@ -14,14 +14,31 @@ Paragraphs should not start with certain bad words.
 
 """
 
-from proselint.registry.checks import Check, types
+import re
+from collections.abc import Iterator
+
+from proselint.registry.checks import Check, CheckResult
+
+
+def _check_but_paragraphs(text: str, check: Check) -> Iterator[CheckResult]:
+    pattern = r"(^|^\n|\n\n)But\b"
+    for m in re.finditer(pattern, text):
+        count = m.group(0).count("\n", 0, 2)
+        yield CheckResult(
+            start_pos=m.start() + count + check.offset[0],
+            end_pos=check.offset[1],
+            check_path=check.path,
+            message=check.message,
+            replacements=None,
+        )
+
 
 check = Check(
-    check_type=types.ExistenceSimple(r"(?:\n\n|^\n)But"),
+    check_type=_check_but_paragraphs,
     path="misc.but",
     message="No paragraph should start with a 'But'.",
     ignore_case=False,
-    offset=(2, 0)
+    offset=(0, 0)
 )
 
 __register__ = (check,)
