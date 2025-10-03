4a06fb5cfe176e523ed8c0e5bd617254d72238a0
nytelife26 <xtylerjrx@gmail.com>
2025-10-03 01:19:40 +0100

    perf: use startswith for fallback in matches_partial
    As @drainpixie noticed in #1440, checking segment sequences is
    equivalent to asking if the raw path starts with the partial key
    followed by the delimiter. This greatly improves the performance of
    matches_partial in the fallback case (3x on my system).
    
    Co-authored-by: drainpixie <121581793+drainpixie@users.noreply.github.com>

--- proselint/registry/checks/__init__.py
+++ proselint/registry/checks/__init__.py
@@ -145,12 +145,7 @@ class Check(NamedTuple):
 
     def matches_partial(self, partial: str) -> bool:
         """Check if `partial` is a subset key of the full check path."""
-        if self.path == partial:
-            return True
-
-        partial_segments = partial.split(".")
-
-        return self.path_segments[: len(partial_segments)] == partial_segments
+        return self.path == partial or self.path.startswith(f"{partial}.")
 
     def check(self, text: str) -> Iterator[CheckResult]:
         """Apply the check over `text`."""
