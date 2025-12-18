505e07c1eed86191a494f45d418e1badf9f23174
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-12-18 19:37:11 +0100

    refactor(nix): clean flake (#1464)
    - Split logic across multiple files in the `nix/` directory
    - Split helpers into a separate file
    - Use lib.getExe{,'} over `{pkgs,derivation}/bin/{name}`

---
+++ nix/checks.nix
@@ -0,0 +1,34 @@
+{
+	lib,
+	self,
+	pkgs,
+	system,
+	...
+}: {
+	pre-commit =
+		self.inputs.hooks.lib.${system}.run {
+			src = ./.;
+			package = pkgs.prek;
+
+			hooks = {
+				trim-trailing-whitespace.enable = true;
+				end-of-file-fixer.enable = true;
+				mixed-line-endings.enable = true;
+				markdownlint.enable = true;
+				alejandra.enable = true;
+				convco.enable = true;
+				ruff.enable = true;
+
+				statix = {
+					enable = true;
+					settings.ignore = ["/.direnv"];
+				};
+
+				pyright = {
+					enable = true;
+					package = pkgs.basedpyright;
+					entry = lib.getExe pkgs.basedpyright;
+				};
+			};
+		};
+}
---
+++ nix/packages.nix
@@ -0,0 +1,13 @@
+{
+	workspace,
+	pythonSet,
+	pkgs,
+	...
+}: {
+	default = pythonSet.mkVirtualEnv "proselint-env" workspace.deps.default;
+
+	wheel = pythonSet.proselint.override {pyprojectHook = pythonSet.pyprojectDistHook;};
+	sdist = (pythonSet.proselint.override {pyprojectHook = pythonSet.pyprojectDistHook;}).overrideAttrs (old: {env.uvBuildType = "sdist";});
+
+	api = pkgs.callPackage ./proselint-api.nix {inherit pythonSet;};
+}
