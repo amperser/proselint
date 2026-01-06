429136a4ce7fd8440d2dca0e6a6181ab582ac6e6
drainpixie <121581793+drainpixie@users.noreply.github.com>
2026-01-06 22:45:04 +0100

    build: website derivation

---
+++ nix/proselint-web.nix
@@ -0,0 +1,51 @@
+{
+	stdenv,
+	nodejs,
+	pnpm,
+	lib,
+	...
+}: let
+	inherit (lib) importJSON fileset;
+	inherit (manifest) name version;
+
+	manifest = importJSON ../website/package.json;
+in
+	stdenv.mkDerivation (final: {
+			inherit version;
+
+			src =
+				fileset.toSource {
+					root = ../.;
+					fileset =
+						fileset.unions [
+							../website
+							../proselint/checks
+						];
+				};
+
+			pname = name;
+			sourceRoot = "source/website";
+
+			nativeBuildInputs = [nodejs pnpm.configHook];
+			pnpmDeps =
+				pnpm.fetchDeps {
+					inherit (final) pname;
+					src = ../website;
+
+					hash = "sha256-kxKZuowOGgxXKMyn66pqkPQv/xgJlnpVE5r5rYyuBOk=";
+					fetcherVersion = 2;
+				};
+
+			postUnpack = ''
+				mkdir -p $sourceRoot/public
+			'';
+
+			buildPhase = ''
+				pnpm run build
+			'';
+
+			installPhase = ''
+				mkdir -p $out/share/proselint-web
+				cp -r dist/* $out/share/proselint-web/
+			'';
+		})
